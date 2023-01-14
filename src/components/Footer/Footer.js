44f3fc57c0436b2837e2f8534727c5ae83f79a53
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-01-14 22:00:51 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,27 @@
 import React from "react";
 import "./Footer.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import EmailIcon from "@mui/icons-material/Email";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <h1>Rent Vroom</h1>
+      <h5>Rent Vroom Pvt. Ltd</h5>
+      <br />
+      <p>
+        No: 296, 3rd Cross Rd, Jakkasandra, 1st Block,
+        <br />
+        Koramangla Bangaluru, Karnataka
+      </p>
+      <div className="icon-wrap">
+        <InstagramIcon />
+        <FacebookIcon />
+        <EmailIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
